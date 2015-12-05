a3aa9cce5d053624994e00241f121545c4062d33
Yefry Calderón <yefry.calderon@udea.edu.co>
2015-12-05 14:15:50 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ubicameUdeaWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ubicameUdeaWebApp
+ */
+angular.module('ubicameUdeaWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
