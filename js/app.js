83ec187a7a4b44bb9872e0c063e3ad3ea1acd2a2
Cristian Correa <ccamilo.correag@gmail.com>
2016-01-17 21:00:35 -0500

    Cambio de la carpeta partials por views. Se cambian imágenes del home. Se agrega el favicon.

--- js/app.js
+++ js/app.js
@@ -7,109 +7,109 @@ angular.module('ubicameUdeaApp', ['ui.router', 'ngResource', 'ubicameUdeaApp.con
 angular.module('ubicameUdeaApp').config(function ($stateProvider, $httpProvider) {
     $stateProvider.state('bloques', {
         url: '/bloques',
-        templateUrl: 'partials/bloques.html',
+        templateUrl: 'views/bloques.html',
         controller: 'BloqueListController'
     }).state('viewBloque', {
 		url: '/bloques/:id/view',
-		templateUrl: 'partials/bloque-view.html',
+		templateUrl: 'views/bloque-view.html',
 		controller: 'BloqueViewController'
     }).state('newBloque', {
         url: '/bloques/new',
-        templateUrl: 'partials/bloque-add.html',
+        templateUrl: 'views/bloque-add.html',
         controller: 'BloqueCreateController'
     }).state('editBloque', {
         url: '/bloques/:id/edit',
-        templateUrl: 'partials/bloque-edit.html',
+        templateUrl: 'views/bloque-edit.html',
         controller: 'BloqueEditController'
     });
 	
 	$stateProvider.state('tiposUnidad', {
         url: '/tiposUnidad',
-        templateUrl: 'partials/tipos_unidad.html',
+        templateUrl: 'views/tipos_unidad.html',
         controller: 'TipoUnidadListController'
     }).state('viewTipoUnidad', {
 		url: '/tiposUnidad/:id/view',
-		templateUrl: 'partials/tipos_unidad-view.html',
+		templateUrl: 'views/tipos_unidad-view.html',
 		controller: 'TipoUnidadViewController'
     }).state('newTipoUnidad', {
         url: '/tiposUnidad/new',
-        templateUrl: 'partials/tipos_unidad-add.html',
+        templateUrl: 'views/tipos_unidad-add.html',
         controller: 'TipoUnidadCreateController'
     }).state('editTipoUnidad', {
         url: '/tiposUnidad/:id/edit',
-        templateUrl: 'partials/tipos_unidad-edit.html',
+        templateUrl: 'views/tipos_unidad-edit.html',
         controller: 'TipoUnidadEditController'
     });
 	
 	$stateProvider.state('unidades', {
         url: '/unidades',
-        templateUrl: 'partials/unidades.html',
+        templateUrl: 'views/unidades.html',
         controller: 'UnidadListController'
     }).state('viewUnidad', {
 		url: '/unidades/:id/view',
-		templateUrl: 'partials/unidades-view.html',
+		templateUrl: 'views/unidades-view.html',
 		controller: 'UnidadViewController'
     }).state('newUnidad', {
         url: '/unidades/new',
-        templateUrl: 'partials/unidades-add.html',
+        templateUrl: 'views/unidades-add.html',
         controller: 'UnidadCreateController'
     }).state('editUnidad', {
         url: '/unidades/:id/edit',
-        templateUrl: 'partials/unidades-edit.html',
+        templateUrl: 'views/unidades-edit.html',
         controller: 'UnidadEditController'
     });
 	
 	$stateProvider.state('departamentos', {
         url: '/departamentos',
-        templateUrl: 'partials/departamentos.html',
+        templateUrl: 'views/departamentos.html',
         controller: 'DepartamentoListController'
     }).state('viewDepartamento', {
 		url: '/departamentos/:id/view',
-		templateUrl: 'partials/departamentos-view.html',
+		templateUrl: 'views/departamentos-view.html',
 		controller: 'DepartamentoViewController'
     }).state('newDepartamento', {
         url: '/departamentos/new',
-        templateUrl: 'partials/departamentos-add.html',
+        templateUrl: 'views/departamentos-add.html',
         controller: 'DepartamentoCreateController'
     }).state('editDepartamento', {
         url: '/departamentos/:id/edit',
-        templateUrl: 'partials/departamentos-edit.html',
+        templateUrl: 'views/departamentos-edit.html',
         controller: 'DepartamentoEditController'
     });
 	
 	$stateProvider.state('ubicaciones', {
         url: '/ubicaciones',
-        templateUrl: 'partials/ubicaciones.html',
+        templateUrl: 'views/ubicaciones.html',
         controller: 'UbicacionListController'
     }).state('viewUbicacion', {
 		url: '/ubicaciones/:id/view',
-		templateUrl: 'partials/ubicaciones-view.html',
+		templateUrl: 'views/ubicaciones-view.html',
 		controller: 'UbicacionViewController'
     }).state('newUbicacion', {
         url: '/ubicaciones/new',
-        templateUrl: 'partials/ubicaciones-add.html',
+        templateUrl: 'views/ubicaciones-add.html',
         controller: 'UbicacionCreateController'
     }).state('editUbicacion', {
         url: '/ubicaciones/:id/edit',
-        templateUrl: 'partials/ubicaciones-edit.html',
+        templateUrl: 'views/ubicaciones-edit.html',
         controller: 'UbicacionEditController'
     });
 	
 	$stateProvider.state('usuarios', {
         url: '/usuarios',
-        templateUrl: 'partials/usuarios.html',
+        templateUrl: 'views/usuarios.html',
         controller: 'UsuarioListController'
     }).state('viewUsuario', {
 		url: '/usuarios/:id/view',
-		templateUrl: 'partials/usuarios-view.html',
+		templateUrl: 'views/usuarios-view.html',
 		controller: 'UsuarioViewController'
     }).state('newUsuario', {
         url: '/usuarios/new',
-        templateUrl: 'partials/usuarios-add.html',
+        templateUrl: 'views/usuarios-add.html',
         controller: 'UsuarioCreateController'
     }).state('editUsuario', {
         url: '/usuarios/:id/edit',
-        templateUrl: 'partials/usuarios-edit.html',
+        templateUrl: 'views/usuarios-edit.html',
         controller: 'UsuarioEditController'
     });
 	
@@ -121,7 +121,7 @@ angular.module('ubicameUdeaApp').config(function ($stateProvider, $httpProvider)
 	
 	$stateProvider.state('home', {
         url: '/home',
-        templateUrl: 'partials/home.html',
+        templateUrl: 'views/home.html',
     });
 	
 }).run(function ($state) {
